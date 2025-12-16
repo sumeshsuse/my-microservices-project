9091651a54a2246126e7958d37460f3e3d21f85d
sumeshsuse <sumeshsuseelan.techie@gmail.com>
2025-12-16 21:36:56 +0100

    sg harbor changes

--- infra/main.tf
+++ infra/main.tf
@@ -1,51 +1,43 @@
-# ------------------------
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# ----------------------------
+# Harbor Module
+# ----------------------------
+module "harbor" {
+  source = "./modules/harbor"
+
+  ami_id               = data.aws_ami.ubuntu.id
+  vpc_id               = data.aws_vpc.default.id
+  key_name             = var.key_name
+  instance_type        = var.instance_type
+  allowed_ssh_cidr     = var.allowed_ssh_cidr
+
+  # 🔴 THIS LINE FIXES YOUR ERROR
+  allowed_client_cidr  = var.allowed_client_cidr
+}
+
+# ----------------------------
 # Data sources
-# ------------------------
+# ----------------------------
 data "aws_vpc" "default" {
   default = true
 }
 
 data "aws_ami" "ubuntu" {
   most_recent = true
+  owners      = ["099720109477"]
 
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
 }
-
-# ------------------------
-# Harbor module
-# ------------------------
-module "harbor" {
-  source = "./modules/harbor"
-
-  ami_id           = data.aws_ami.ubuntu.id
-  vpc_id           = data.aws_vpc.default.id
-  key_name         = var.key_name
-  instance_type    = var.instance_type
-  allowed_ssh_cidr = var.allowed_ssh_cidr
-}
-
-# ------------------------
-# Kubernetes cluster module
-# ------------------------
-module "k8s_cluster" {
-  source = "./modules/k8s-cluster"
-
-  ami_id                = data.aws_ami.ubuntu.id
-  vpc_id                = data.aws_vpc.default.id
-  key_name              = var.key_name
-  allowed_ssh_cidr      = var.allowed_ssh_cidr
-  control_instance_type = var.control_instance_type
-  worker_instance_type  = var.worker_instance_type
-  worker_count          = var.worker_count
-}
-
