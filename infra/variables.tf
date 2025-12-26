9e39c21cbe6b011deac13a4d9089b1d53f442e96
sumeshsuse <sumeshsuseelan.techie@gmail.com>
2025-12-27 00:49:33 +0530

    changes

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,43 +1,28 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-}
-
 variable "key_name" {
+  type        = string
   description = "EC2 key pair name"
-  type        = string
 }
 
-# Harbor EC2 instance type
 variable "instance_type" {
-  description = "Instance type for Harbor EC2"
   type        = string
+  description = "Instance type for Harbor instance"
+  default     = "t3.medium"
 }
 
-variable "allowed_ssh_cidr" {
-  description = "CIDR allowed to SSH into instances"
-  type        = string
-}
-
-variable "allowed_client_cidr" {
-  description = "CIDR allowed for Docker HTTPS probe on 443 (your public IP /32)"
-  type        = string
-}
-
-# ----------------------------
-# K8s cluster variables
-# ----------------------------
 variable "k8s_control_instance_type" {
-  description = "Instance type for Kubernetes control plane"
   type        = string
+  description = "Instance type for Kubernetes control plane"
+  default     = "t3.medium"
 }
 
 variable "k8s_worker_instance_type" {
-  description = "Instance type for Kubernetes worker nodes"
   type        = string
+  description = "Instance type for Kubernetes worker nodes"
+  default     = "t3.medium"
 }
 
 variable "k8s_worker_count" {
-  description = "Number of Kubernetes worker nodes"
   type        = number
+  description = "Number of Kubernetes worker nodes"
+  default     = 2
 }
